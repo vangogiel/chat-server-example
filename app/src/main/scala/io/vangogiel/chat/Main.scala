fee5f6cd56efa33ee880944bd872596d29001b42
Norbert Gogiel <norbert.gogiel@iceservices.com>
2024-09-27 12:43:32 +0100

    Add server and stub responses

---
+++ app/src/main/scala/io/vangogiel/chat/Main.scala
@@ -0,0 +1,25 @@
+package io.vangogiel.chat
+
+import cats.effect.{ExitCode, IO, IOApp}
+import io.grpc.protobuf.services.ProtoReflectionService
+import io.grpc.netty.shaded.io.grpc.netty.NettyServerBuilder
+import io.vangogiel.chat.chat_service.ChatServiceFs2Grpc
+
+object Main extends IOApp {
+  def run(args: List[String]): IO[ExitCode] = {
+    createServerAndAddServices()
+      .evalMap(server => IO(server.build().start()))
+      .useForever
+      .map(_ => ())
+      .as(ExitCode.Success)
+  }
+
+  private def createServerAndAddServices() = {
+    val serverBuilder = NettyServerBuilder.forPort(9999)
+    for {
+      chatService <- ChatServiceFs2Grpc.bindServiceResource(new ChatServiceImpl[IO])
+      _ = serverBuilder.addService(chatService)
+      _ = serverBuilder.addService(ProtoReflectionService.newInstance())
+    } yield serverBuilder
+  }
+}
